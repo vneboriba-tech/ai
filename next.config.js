6918159b1c12fc33af157e93efc5358eae458a0e
vneboriba-tech <vneboriba@gmail.com>
2025-10-14 02:57:15 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
-
-const repoEnv = process.env.GITHUB_REPOSITORY || ""; // e.g. username/repo
-const [username, repo] = repoEnv.split("/");
-
 module.exports = {
   output: "export",
   images: { unoptimized: true },
-  basePath: repo ? `/${repo}` : "",
-  assetPrefix: username && repo ? `https://${username}.github.io/${repo}` : "",
 };

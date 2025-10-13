a1857f50f38a189faba12521b6bb542d422efcc2
vneboriba-tech <vneboriba@gmail.com>
2025-10-14 02:05:04 +0300

    Create page.tsx

---
+++ mini_site_media_professions_ready_pages/mini_site_media_professions_ready_pages/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <main style={{ textAlign: "center", padding: "80px 20px", fontFamily: "sans-serif" }}>
+      <h1 style={{ fontSize: "2em", marginBottom: "20px" }}>Почувствуй медиа</h1>
+      <p style={{ fontSize: "1.2em" }}>Мини-сайт Медиастарт 2025</p>
+      <p>Факультет журналистики МГУ</p>
+      <p>С любовью, КМ ❤️</p>
+    </main>
+  );
+}

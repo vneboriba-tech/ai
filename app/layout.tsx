844792bef957ce859c03703c2bc55ec633c84325
vneboriba-tech <vneboriba@gmail.com>
2025-10-14 02:56:05 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ru">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </head>
+      <head><meta name="viewport" content="width=device-width, initial-scale=1" /></head>
       <body>{children}</body>
     </html>
   );
